c72bc7d0390c60a5e9a7b82988b6db75903a9d1c
Cathy Xiao <cathyx125@hotmail.com>
2020-09-17 14:55:31 -0400

    finished assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix function is used to create and store a matrix.
+## cacheSolve function is used to invert the matrix
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    if(ncol(x) == nrow(x)){
+      i <- NULL
+      set <- function(y){
+        x <<- y
+        i <<- NULL
+      }
+      
+      get <- function() x
+      setinversion <- function(inversion) i <<- inversion
+      getinversion <- function() i
+      list(set = set, get = get, setinversion = setinversion, getinversion = getinversion)
+      
+    }else{
+      message("This is an uninversitable matrix.")
+      i <<- NULL
+    }
 }
 
 
@@ -12,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    i <- x$getinversion()
+    if(!is.null(i)){
+      message("getting the inverse of a matrix")
+      return(i)
+    }
+    data <- x$get()
+    i <- solve(data, matrix(1, nrow = nrow(data), ncol = ncol(data)), ...)
+    x$setinversion(i)
+    i
 }
